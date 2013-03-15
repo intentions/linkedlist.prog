0e98efb9c04595cc3038ac1552a20359ec879b40
Kurt J Strosahl <kjstrosahl@gmail.com>
2013-03-14 21:20:17 -0400

    initial linked list

---
+++ linkedlist.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int x;
+	struct node *next;
+}
+
+int main(void)
+{
+	//root node
+	struct node *root
+
+	//point root to a node struct
+	root = malloc( sizeof(struct node) );
+
+	
+}
